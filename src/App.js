a0d7198b02b4bb3f9e252edc9dd96b92b83dcf65
johnalid <johnalidy05@gmail.com>
2021-03-05 14:31:16 -0700

    k

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
 import Home from './Home'
-import Nav from './Nav'
+// import Nav from './Nav'
 
 function App() {
   return (
     <div className="App">
-    <Nav/>
+    {/* <Nav/> */}
     <Home/>  
        
   
